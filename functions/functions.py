dd57e3b576ad1fe35886603609574465e0249c26
Camila Franco de Sousa <imcamilafranco@gmail.com>
2019-06-27 00:55:43 +0000

    functions - rasp

---
+++ functions/functions.py
@@ -0,0 +1,20 @@
+def serial_communication():
+    #!/usr/bin/env/python
+    # -*- coding: utf-8 -*-
+    import serial
+    port = "/dev/ttyUSB0"
+    rate = 9600
+    s1 = serial.Serial(port, rate)
+    s1.flushInput()
+    return
+
+def interrupt_to_esp():
+    serial_communication()
+    comp_list = ["Interrupt\r\n"]
+    while True:
+        if s1.inWaiting()>0:
+            inputValue = s1.readline()
+            print(inputValue)
+            if inputValue in comp_list:
+                s1.write('0')
+                return
